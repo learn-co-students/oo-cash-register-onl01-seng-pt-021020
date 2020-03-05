e226c8f769df909841a63eb7cfbd4205086397eb
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-03-05 17:30:59 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+class CashRegister
+attr_accessor :total, :discount, :items, :price
+item = []
 
+def initialize(discount = 0)
+  @total = 0
+  @discount = discount
+  @item = []
+end
+
+def apply_discount
+  if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - @total * @discount
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+end
+end
+#we need to pass accept item name and a value of the item  and increase of total,
+#accept an optional quantity
+#don't forget about the total#
+def add_item(title, price, quantity = 0)
+  if quantity > 1
+    i=0
+    while i < quantity
+      @item << title
+      i+=1
+    end
+  else @item << title
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+end
+end
+
+def void_last_transaction()
+    @total -=
+    @last_transaction_amount
+  end
+end
